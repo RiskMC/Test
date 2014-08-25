d625d117b10f29e90accad5874cfd4098eb91efd
GameMaster2468 <Brandons Account@user-PC.home>
2014-08-25 14:23:09 -0400

    First test commit.

--- Test/src/com/riskmc/test/Test.java
+++ Test/src/com/riskmc/test/Test.java
@@ -4,19 +4,12 @@
  * and open the template in the editor.
  */
 
-package test;
+package com.riskmc.test;
 
 /**
  *
  * @author Brandons Account
  */
 public class Test {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
